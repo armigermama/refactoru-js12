066623893a428b53be43d390804572e473bc0247
Roni.Chen <roni.chen@comcast.net>
2014-01-27 16:51:06 -0700

    set rating implemented

--- main.js
+++ main.js
@@ -9,54 +9,81 @@ $(function(){
 		quote.author = $('.author').val();
 		quote.rating = 0;
 		quote.deleted = false;
-		quote.timeStamp = 'quote_' + new Date().getTime().toString();
+		quote.timeStamp = new Date().getTime();
 		quoteArray.push(quote);
-		console.log(quoteArray);
+		// console.log(quoteArray);
 		renderList();
 	});
-
-
 
 	var renderList = function(){
 		
 		var buildLi = function(quoteObj){
-			var li = $('<li>\
+			var li = '<li data-timestamp={timeStamp}>\
 						<section>\
 						<blockquote>\
 						<p>\"{quote}\"</p>\
 						</blockquote>\
 						<div class=\"clearfix\">\
 						<div class=\"float-right\">{author}</div>\
-						<div class=\"float-left\">\
-						<img src=\"images/star-white.png\">\
-						<img src=\"images/star-white.png\">\
-						<img src=\"images/star-white.png\">\
-						<img src=\"images/star-white.png\">\
-						<img src=\"images/star-white.png\">\
-						</div>\
+						<div class=\"float-left\">'.supplant(quoteObj);
+				for (var i=0; i<quoteObj.rating; i++) {
+					li+='<img class=\"star\" src=\"images/star-yellow.png\">';
+				}
+
+				for (i=quoteObj.rating; i<5; i++) {
+					li+='<img class=\"star\" src=\"images/star-white.png\">';
+				}
+
+					li+='</div>\
 						</div>\
 						</section>\
-						</li>'.supplant(quoteObj));
-			return li;
+						</li>';
+						// console.log(li);
+			return $(li);
 		};
 
 		$('#quote-list').empty();
-		
-		for (i=0; i<quoteArray.length; i++) {
-			$('#quote-list').prepend(buildLi(quoteArray[i]));
+
+		quoteArray.sort(function(a,b){
+				//TODO  maybe sort by obj.timestamp
+				if (a.rating === b.rating){
+					if (a.timeStamp < b.timeStamp)
+						return 1;
+					if (a.timeStamp > b.timeStamp)
+						return -1;
+					return 0;
+				}
+				if (a.rating < b.rating)
+					return 1;
+				if (a.rating > b.rating)
+					return -1;
+				return 0;
+			
+		});
+
+	
+		console.log(quoteArray);
+
+		for (var j=0; j<quoteArray.length; j++) {
+			//console.log(quoteArray.length);
+			$('#quote-list').prepend(buildLi(quoteArray[j]));
 		}
 	};
 
+	$(document).on('click','.star',function(){
+		var timeStamp = parseInt($(this).closest('li').attr('data-timestamp'));
+				//console.log(timeStamp);
+				//console.log(quoteArray[0].timeStamp);
+		for (var k=0; k<quoteArray.length; k++) {
+			if (quoteArray[k].timeStamp === timeStamp){
+
+				quoteArray[k].rating = ($(this).parent().find("img").index(this) + 1);
+			}
 
 
-
-
-
-
-
-
-
-
+		//console.log(quoteArray[k].rating);
+		}
+	});
 
 
 
