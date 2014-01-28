0e4b4cd7eac39697cc164d58dd95000aab16408a
Roni.Chen <roni.chen@comcast.net>
2014-01-28 12:25:25 -0700

    modified styles & author filter js

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ $(function(){
 		$('#quote-list').empty();
 
 		quoteArray.sort(function(a,b){
-				//TODO  maybe sort by obj.timestamp
 				if (a.rating === b.rating){
 					if (a.timeStamp < b.timeStamp)
 						return 1;
@@ -31,7 +30,6 @@ $(function(){
 		console.log(quoteArray);
 
 		for (var j=0; j<quoteArray.length; j++) {
-			//console.log(quoteArray.length);
 			$('#quote-list').append(buildLi(quoteArray[j]));
 		}
 	};
@@ -43,7 +41,7 @@ $(function(){
 					<p>\"{quote}\"</p>\
 					</blockquote>\
 					<div class=\"clearfix\">\
-					<div class=\"float-right author-filter\">{author}</div>\
+					<div class=\"float-right author-filter\">- {author} -</div>\
 					<div class=\"float-left\">'.supplant(quoteObj);
 			for (var i=0; i<quoteObj.rating; i++) {
 				li+='<img class=\"star\" src=\"images/star-yellow.png\">';
@@ -57,7 +55,6 @@ $(function(){
 					</div>\
 					</section>\
 					</li>';
-					// console.log(li);
 		return $(li);
 	};
 
@@ -74,7 +71,6 @@ $(function(){
 		quote.deleted = false;
 		quote.timeStamp = new Date().getTime();
 		quoteArray.push(quote);
-		// console.log(quoteArray);
 		renderList();
 	});
 
@@ -82,26 +78,24 @@ $(function(){
 // and replacing parent li with newly rendered li
 	$(document).on('click','.star',function(){
 		var timeStamp = parseInt($(this).closest('li').attr('data-timestamp'));
-				//console.log(timeStamp);
-				//console.log(quoteArray[0].timeStamp);
 		for (var k=0; k<quoteArray.length; k++) {
 			if (quoteArray[k].timeStamp === timeStamp){
 				quoteArray[k].rating = ($(this).parent().find("img").index(this) + 1);
 				$(this).closest("li").replaceWith(buildLi(quoteArray[k]));
 			}
 
-
-		//console.log(quoteArray[k].rating);
 		}
 	});
 
-		$(document).on('click','.author-filter',function(){
-			//console.log($(this).text());
-			//$(this);
-			var containsAuthor = $(".author-filter:contains(" + $(this).text() + ")").closest("li");
-			console.log(containsAuthor);
-			var notContainsAuthor = $("#quote-list li").not(containsAuthor);
-			$(notContainsAuthor).toggle();
+	$(document).on('click','.author-filter',function(){
+		// var containsAuthor = $(".author-filter:contains(" + $(this).text() + ")").closest("li");
+		// console.log(containsAuthor);
+		// var notContainsAuthor = $("#quote-list li").not(containsAuthor);
+		// $(notContainsAuthor).toggle();
+		var author = $(this).text();
+		$('.author-filter').filter(function(){
+			return $(this).text() !== author;
+		}).closest('li').toggle();
 
 
 	});
